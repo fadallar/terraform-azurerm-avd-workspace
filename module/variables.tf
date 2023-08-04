c84986af5fcaa770b0f49b5da6ee3bc7168ff03a
Fabrice Dall'ara <fabrice.dallara@hotmail.com>
2023-08-04 09:01:14 +0200

    Reorganizing variables in correct variables files and adding validation block

--- module/variables.tf
+++ module/variables.tf
@@ -9,20 +9,6 @@ variable "location" {
   type        = string
 }
 
-variable "location_short" {
-  description = "Short string for Azure location."
-  type        = string
-}
-
-variable "stack" {
-  description = "Project stack name."
-  type        = string
-  validation {
-    condition     = var.stack == "" || can(regex("^[a-z0-9]([a-z0-9-]*[a-z0-9])?$", var.stack))
-    error_message = "Invalid variable: ${var.stack}. Variable name must start with a lowercase letter, end with an alphanumeric lowercase character, and contain only lowercase letters, digits, or a dash (-)."
-  }
-}
-
 variable "friendly_name" {
   type        = string
   description = "A friendly name for the Virtual Desktop Workspace."
